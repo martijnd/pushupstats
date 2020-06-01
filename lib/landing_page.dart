2c69980c817f033c56014fbeb1fa9507f6c55c2d
martijnd <martijn.dorsman@gmail.com>
2020-06-01 16:20:23 +0200

    Added functionality

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/date_symbol_data_file.dart';
 import 'package:pushupstats/database.dart';
 import 'package:intl/intl.dart';
 import 'sign_in.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 final databaseReference = Firestore.instance;
 
@@ -18,6 +18,11 @@ class FirstScreen extends StatefulWidget {
 @override
 class _FirstScreenState extends State<FirstScreen> {
   int amount;
+  String _date = DateFormat('yyyy-MM-dd').format(DateTime.now());
+  String _formattedDate = DateFormat('dd-MM-yyyy').format(DateTime.now());
+  String _time = DateFormat('hh:mm').format(DateTime.now());
+
+  TextEditingController _controller = TextEditingController();
 
   void initState() {
     super.initState();
@@ -48,7 +53,7 @@ class _FirstScreenState extends State<FirstScreen> {
               Container(
                 padding: EdgeInsets.all(20.0),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
                     Text(
                       'Amount of pushups',
@@ -56,11 +61,139 @@ class _FirstScreenState extends State<FirstScreen> {
                           fontSize: 30.0, fontWeight: FontWeight.bold),
                     ),
                     SizedBox(height: 20),
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                      elevation: 4.0,
+                      onPressed: () {
+                        DatePicker.showDatePicker(context,
+                            theme: DatePickerTheme(
+                              containerHeight: 210.0,
+                            ),
+                            showTitleActions: true,
+                            minTime: DateTime(2000, 1, 1),
+                            maxTime: DateTime(2022, 12, 31), onConfirm: (date) {
+                          print('confirm $date');
+                          _formattedDate =
+                              '${DateFormat("dd").format(date)}-${DateFormat("MM").format(date)}-${DateFormat("yyyy").format(date)}';
+                          _date =
+                              '${DateFormat("yyyy").format(date)}-${DateFormat("MM").format(date)}-${DateFormat("dddd").format(date)}';
+                          setState(() {});
+                        }, currentTime: DateTime.now(), locale: LocaleType.en);
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 50.0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.date_range,
+                                        size: 18.0,
+                                        color: Colors.blue[700],
+                                      ),
+                                      Text(
+                                        " $_formattedDate",
+                                        style: TextStyle(
+                                            color: Colors.blue[700],
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18.0),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                            Text(
+                              "Change",
+                              style: TextStyle(
+                                  color: Colors.blue[700],
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                      color: Colors.white,
+                    ),
+                    SizedBox(height: 10),
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0)),
+                      elevation: 4.0,
+                      onPressed: () {
+                        DatePicker.showTimePicker(context,
+                            showSecondsColumn: false,
+                            theme: DatePickerTheme(
+                              containerHeight: 210.0,
+                            ),
+                            showTitleActions: true, onConfirm: (time) {
+                          print('confirm $time');
+                          _time = '${time.hour}:${time.minute}';
+                          setState(() {});
+                        }, currentTime: DateTime.now(), locale: LocaleType.en);
+                        setState(() {});
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 50.0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.access_time,
+                                        size: 18.0,
+                                        color: Colors.blue[700],
+                                      ),
+                                      Text(
+                                        " $_time",
+                                        style: TextStyle(
+                                            color: Colors.blue[700],
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18.0),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                            Text(
+                              "Change",
+                              style: TextStyle(
+                                  color: Colors.blue[700],
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                      color: Colors.white,
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Amount of pushups',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
                     TextField(
+                      controller: _controller,
                       onSubmitted: (String value) async {
                         if (value.length != 0) {
-                          await DatabaseService(uid: uid)
-                              .addPushup(int.parse(value), DateTime.now());
+                          await DatabaseService(uid: uid).addPushup(
+                              int.parse(value),
+                              DateTime.parse('${_date}T$_time:00Z'));
+
+                          _controller.clear();
                         }
                       },
                       keyboardType: TextInputType.number,
